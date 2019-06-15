8d3e02253728cc4c082eb51c8f80eb4ac5e6eb11
mmi366127 <35695972+mmi366127@users.noreply.github.com>
2019-06-16 00:07:11 +0800

    Update template.cpp

--- TIOJ/template.cpp
+++ TIOJ/template.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
-#define all(X) X.begin(), X.end()
-#define maxn
-typedef long long loli;
+#define X first
+#define Y second
+#define all(X) (X).begin(), (X).end()
+#define rall(X) (X).rbegin(), (X).rend()
+#define lowbit(X) ((X)&-(X))
+#define MEM(X, val) memset((X), (val), sizeof(X));
+#define pb push_back
+#define ins insert
+#define mp make_pair
+#define YEE ios_base::sync_with_stdio(0); cin.tie()
+#define maxn 100007
 using namespace std;
+typedef long long loli;
+typedef pair<int,int> pii;
+typedef pair<loli,loli> pll;
+
+
+const loli INF = 0x3f3f3f3f3f3f3f3f;
+
 
 
 int main() {
