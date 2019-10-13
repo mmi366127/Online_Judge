a75183d156de7ec2f53cd2a38122992040cc7c6e
mmi366127 <35695972+mmi366127@users.noreply.github.com>
2019-10-13 20:21:58 +0800

    Update disjoint_set.cpp

--- template/disjoint_set.cpp
+++ template/disjoint_set.cpp
@@ -4,21 +4,22 @@
 using namespace std;
 
 struct DS {
-    vector<int> p,s;
+    vector<int> p, s;
     int sz;
-    DS(int _sz) : sz(_sz), p(_sz+1,-1), s(_sz+1,1) {}
+    DS(int _sz) : sz(_sz), p(_sz + 1, -1), s(_sz + 1, 1) {}
     int find(int x){
-        return (p[x]<0) ?  x : (p[x] = find(p[x]));
+        return (p[x] < 0) ?  x : (p[x] = find(p[x]));
     }
-    void join(int x,int y){
+    bool join(int x,int y){
         x = find(x);
         y = find(y);
-        if(x==y)
-            return;
-        if(s[x]<s[y])
-            swap(x,y);
+        if(x == y)
+            return false;
+        if(s[x] < s[y])
+            swap(x, y);
         p[y] = x;
         s[x] += s[y];
+        return true;
     }
     int size(int x){
         x = find(x);
@@ -28,6 +29,6 @@ struct DS {
 
 
 int main(){
-    DS  my_dis_joint_set(87);
+    
     return 0;
 }
